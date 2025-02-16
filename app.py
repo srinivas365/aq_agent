75f6e1e774987f4f21aa97560fcb7d7b95d45454
srinivas-tanla <srinivas.mannem@tanla.com>
2025-02-16 11:04:32 +0530

    updated groq_bot and storing the conversation logs in mongodb

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import pymongo
+
 from notify_service import SlackNotifier
 from ticket_service import JiraTicketService
 from dotenv import  load_dotenv
@@ -27,9 +29,11 @@ if __name__ == '__main__':
     # initializing the vectordb service
     dbservice = VectorDBService()
 
-    # initializing the bot
-    bot = GroqBot()
+    # initialize mongodb client
+    dbclient = pymongo.MongoClient(os.environ.get("MONGO_URI"))
 
+    # initializing the bot
+    bot = GroqBot(dbclient, notifier)
 
     while True:
         query = input("report query[enter exit to close]:")
@@ -39,38 +43,16 @@ if __name__ == '__main__':
         for report in results.objects:
             print(f"uuid: {report.uuid}, report: {report.properties['name']}, similarity_score: {report.metadata.distance}")
 
-        feedback = input("Are you satified with the results[agent/ai]:")
-        if feedback.lower() == 'ai':
+        feedback = input("Are you satified with the results[y/n]:")
+        if feedback.lower() == 'n':
             print("Starting Groq chat for further details...")
             bot.collect_report_details()
             bot.confirm_and_summarize()
-
-        if feedback.lower() == "agent":
-            print("escalating to team......")
-            report_text = input("Describe your report in detail:")
-            year = input("Any specific year:")
-            add_info = input("Any additional info:")
-            mail_id = input("your mailId:")
-            ticket_id = str(uuid.uuid4())
-            notification_msg = f'''
-            User has queried about the report with attached information.
-            ticket_id: {ticket_id}
-            report: {report_text}
-            year: {year}
-            additional_info: {add_info}
-            requested by: {mail_id}
-            '''
-            notifier.send_notification({"text": notification_msg})
-            ticket = {
-                "report_text": report_text,
-                "year": year,
-                "add_info": add_info,
-                "mail_id": mail_id,
-                "ticket_id": ticket_id,
-            }
-            tkt_service.create_ticket(ticket)
-            print(f"ticket has been created with id {ticket_id}. Team will reach you shortly...")
+            ticket_id = bot.report_id
+            tkt_service.create_ticket({"ticket_id": ticket_id, "report_details": bot.report_details})
+            print(f"Ticket has been created with id {ticket_id}. Team will reach you shortly...")
 
     print(f"Closing the application")
     dbservice.close()
+    dbclient.close()
 
